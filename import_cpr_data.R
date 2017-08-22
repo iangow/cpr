a78c0a90ec473b5933702cfc3818e0ab494c657b
bhaskarv275 <bhaskarv275@gmail.com>
2017-08-22 13:25:14 +1000

    no message

--- import_cpr_data.R
+++ import_cpr_data.R
@@ -9,3 +9,14 @@ cpr_data
 cpr_data %>% select(date)
 
 ## cpr data imported
+
+cpr_data %>% mutate(date=as.Date(date))
+
+library(stringr)
+
+fix_boolean <- function(var){
+    if_else(str_detect(var,"^Yes"),TRUE,if_else(str_detect(var,"^No"),FALSE,NA))
+}
+
+cpr_data %>% mutate(us_public_co_note=us_public_co,us_public_co=fix_boolean(us_public_co)) %>% count(us_public_co_note,us_public_co)
+
